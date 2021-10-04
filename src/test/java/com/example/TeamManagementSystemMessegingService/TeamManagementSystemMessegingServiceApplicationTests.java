dfee438643d335e6c266240d9ea6586a3d909cc4
andriy-mykytyn <andrik.mykytyn@gmail.com>
2021-10-05 00:23:12 +0300

    Initial commit

---
+++ src/test/java/com/example/TeamManagementSystemMessegingService/TeamManagementSystemMessegingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeamManagementSystemMessegingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamManagementSystemMessegingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
